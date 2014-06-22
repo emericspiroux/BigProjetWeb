e5a55042ebaf23e5c596d96add5fd85dee5f695c
Emeric SPIROUX <espiroux@e1r6p10.42.fr>
2014-06-22 19:33:22 +0200

    site web avec test --all

--- intra/application/views/lang.php
+++
@@ -1,9 +0,0 @@
-<div id="langue" style='border:1px solid black;width:100px;margin-top:10px;padding:10px;'>
-<p>Language:
-<?php
- if ($langue != 'fr')
- 		echo anchor(base_url().'framework/change_lang/fr', 'fran&ccedil;ais');
- if ($langue != 'eng')
- 		echo anchor(base_url().'framework/change_lang/eng', 'english');
-?></p>
-</div>
